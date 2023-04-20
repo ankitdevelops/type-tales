053c1dfc9056616f0c772aa79fca5a622ca13ad1
Ankit Kumar <46848486+ankitdevelops@users.noreply.github.com>
2023-04-20 10:41:47 +0530

    story model

---
+++ src/models/post.schema.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const storySchema = new mongoose.Schema(
+  {
+    story: {
+      type: String,
+      required: true,
+      maxLength: [500, "Maximum 500 character allowed"],
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Story", taleSchema);
